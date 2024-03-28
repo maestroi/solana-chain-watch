a6ca38df4184d4d4e3370c8c9b3bf8921cd792c0
Chuck Veenvliet <cveenvliet@blockdaemon.com>
2024-03-28 10:01:29 +0100

    chore(): update getblock

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ async def fetch_block(session, url, slot, retries=4, delay=0.1):
             "method": "getBlock",
             "params": [slot, {
                 "encoding": "json",
+                "maxSupportedTransactionVersion":0,
                 "transactionDetails": "full",
                 "rewards": False,
                 "commitment": "finalized"
@@ -100,8 +101,8 @@ async def main():
         node_url = config_url.split('//')[1].split('/')[0]
         node_ip = socket.gethostbyname(node_url)
         # Synchronous ping call - consider finding an async solution
-        #ping_time = ping(node_ip) @TODO FIX ME requires root
-        ping_time = 30
+        ping_time = ping(node_ip) #@TODO FIX ME requires root
+        #ping_time = 30
         logging.info(40 * "-")
         logging.info(f"Latency checker for Solana node")
         logging.info(f"Ping time to node: {ping_time * 1000:.2f}ms")
